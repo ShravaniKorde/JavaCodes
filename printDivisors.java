2ab914a546fa734d19c90f4394798757408c8e6e
Shravani Korde <149994109+ShravaniKorde@users.noreply.github.com>
2025-10-04 19:41:12 +0530

    solution added

--- printDivisors.java
+++ printDivisors.java
@@ -26,3 +26,22 @@ public class printDivisors {
     }
     
 }
+
+---------------------------------------OR-----------------------------------------------
+    class Solution {
+    public static void print_divisors(int n) {
+        
+        for(int i = 1; i*i< n;i++){
+            if(n%i==0){
+                System.out.print(i + " ");
+            }
+        }
+        for(int i=(int)Math.sqrt(n); i>=1; i--){
+            if(n%i==0){
+                System.out.print(n/i + " ");
+            }
+        }
+        
+    }
+}
+
