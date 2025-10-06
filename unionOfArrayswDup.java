724930147e67d5583e4f54066cdc82b8875f0955
Shravani Korde <149994109+ShravaniKorde@users.noreply.github.com>
2025-10-06 23:33:27 +0530

    solution added

---
+++ unionOfArrayswDup.java
@@ -0,0 +1,14 @@
+class Solution {
+    public static ArrayList<Integer> findUnion(int[] a, int[] b) {
+        // code here
+        HashSet<Integer> set = new HashSet<>();
+        for(int x : a){
+            set.add(x);
+        }
+        for(int x: b){
+            set.add(x);
+        }
+        ArrayList<Integer> res = new ArrayList<>(set);
+        return res;
+    }
+}
