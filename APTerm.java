c65318923a8abf54062a9b20409548d0f3fa3b11
Shravani Korde <149994109+ShravaniKorde@users.noreply.github.com>
2025-10-08 21:28:36 +0530

    solution added

---
+++ APTerm.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int apTerm(int a, int d, int n) {
+       
+        
+        int ans = a+(n-1)*d; // a is 1st term, d is common diff, nth term is calculated as shown 
+
+            return ans;
+    }
+}
