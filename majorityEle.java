8b79f5d3dbeabc04bebf408071dc0ef28e2b79c6
Shravani Korde <149994109+ShravaniKorde@users.noreply.github.com>
2025-10-09 21:21:15 +0530

    solution added

---
+++ majorityEle.java
@@ -0,0 +1,20 @@
+class Solution {
+    int majorityElement(int arr[]) {
+        // code here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0;i<arr.length;i++){
+            if(map.containsKey(arr[i]))
+            {
+                map.put(arr[i], map.get(arr[i])+1);
+            }
+            else{
+                map.put(arr[i], 1);
+            }
+        }
+        for(Map.Entry<Integer, Integer> entry:map.entrySet()){
+            if(entry.getValue() > arr.length /2) return entry.getKey();
+        }
+        return -1;
+    }
+}
