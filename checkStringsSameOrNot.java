95da7f41085618786b09ee74802fab66d20be309
Shravani Korde <149994109+ShravaniKorde@users.noreply.github.com>
2025-12-04 19:00:58 +0530

    solution added

---
+++ checkStringsSameOrNot.java
@@ -0,0 +1,12 @@
+class StringUtils {
+   
+    public static boolean areStringsSame(String s1, String s2) {
+        if(s1.length()!=s2.length()) return false;
+        
+        for(int i = 0; i<s1.length(); i++){
+            if(s1.charAt(i)!=s2.charAt(i))  
+                return false;     
+        }
+        return true;
+    }
+}
